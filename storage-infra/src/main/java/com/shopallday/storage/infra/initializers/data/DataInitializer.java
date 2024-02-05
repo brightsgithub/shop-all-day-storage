37f11f0de0858830d546de8b9007db2b643d597e
BrightPersonal <brightpersonal@Brights-Mac-mini.local>
2024-02-05 20:22:33 +0000

    testing Transaction attribute. Now works with checked exception

--- storage-infra/src/main/java/com/shopallday/storage/infra/initializers/data/DataInitializer.java
+++ storage-infra/src/main/java/com/shopallday/storage/infra/initializers/data/DataInitializer.java
@@ -6,6 +6,7 @@ import com.shopallday.storage.domain.initializers.StorageInitializer;
 import com.shopallday.storage.domain.models.Customer;
 import com.shopallday.storage.domain.usecases.CreateCustomersUseCase;
 import com.shopallday.storage.domain.usecases.GetAllCustomersUseCase;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -25,10 +26,21 @@ public class DataInitializer implements StorageInitializer {
     }
 
 
+    /**
+     * By default, Spring's declarative transaction management only rolls back transactions for unchecked exceptions
+     * (runtime exceptions).
+     * If CreateCustomerException is a checked exception, it won't trigger a rollback unless you explicitly configure
+     * it using rollbackOn attributes in the @Transactional annotation.
+     * The below will now rollback on a Runtime exception or CreateCustomerException
+     * @throws CreateCustomerException
+     * @throws ReadCustomerException
+     */
+    @Transactional(rollbackOn = { CreateCustomerException.class })
     @Override
     public void initialize() throws CreateCustomerException, ReadCustomerException {
         System.out.println("DataInitializer called");
         createCustomers();
+        //testRollBack();
         printCustomers();
         System.out.println("DataInitializer finished");
     }
@@ -37,6 +49,10 @@ public class DataInitializer implements StorageInitializer {
         System.out.println("createCustomers called...");
         createCustomersUseCase.execute(getTestCustomers());
         System.out.println("createCustomers finished");
+    }
+
+    private void testRollBack() throws CreateCustomerException {
+        throw new CreateCustomerException("Testing rollback");
     }
 
     private void printCustomers() throws ReadCustomerException {
